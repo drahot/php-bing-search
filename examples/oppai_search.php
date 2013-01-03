031e808117c3894bb660e84f66a3cd61d88872b2
Tatusya Hotta <hotterdragon@gmail.com>
2013-01-03 12:05:16 +0900

    Examples Add And README.md Modified.

---
+++ examples/oppai_search.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Bing\Search\Factory;
+
+$accountKey = "YOUR ACCOUNT KEY";
+$factory = new Factory($accountKey);
+$s = $factory->createImageSearch('oppai');
+
+foreach ($s as $key => $resultObject) {
+	foreach ($resultObject as $obj) {
+		echo $obj, PHP_EOL;
+	}
+}
+
